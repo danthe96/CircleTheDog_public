901136dc88e707992a1611cdbddb54b93d6967ce
Zompa <...>
2014-11-08 00:00:39 +0100

    One More Thing

--- CircleTheDog/src/com/danthe/dogeescape/PlayerArt.java
+++ CircleTheDog/src/com/danthe/dogeescape/PlayerArt.java
@@ -1,5 +1,17 @@
 package com.danthe.dogeescape;
 
-public class PlayerArt {
+import org.andengine.entity.sprite.AnimatedSprite;
+import org.andengine.entity.sprite.vbo.ITiledSpriteVertexBufferObject;
+import org.andengine.opengl.texture.region.ITiledTextureRegion;
+
+public class PlayerArt extends AnimatedSprite{
+
+	public PlayerArt(float pX, float pY, float pWidth, float pHeight,
+			ITiledTextureRegion pTiledTextureRegion,
+			ITiledSpriteVertexBufferObject pTiledSpriteVertexBufferObject) {
+		super(pX, pY, pWidth, pHeight, pTiledTextureRegion,
+				pTiledSpriteVertexBufferObject);
+		// TODO Auto-generated constructor stub
+	}
 
 }
