70a59ba0b1af3b5d1b3159ef56693ec23da2e9bb
danthe96 <danthe96@gmx.de>
2014-11-12 02:08:01 +0100

    Just one line of code, forgot to load text box texture.

--- CircleTheDog/src/com/danthe/dogeescape/GameActivity.java
+++ CircleTheDog/src/com/danthe/dogeescape/GameActivity.java
@@ -123,6 +123,7 @@ public class GameActivity extends SimpleBaseGameActivity implements
 
 			// 2 - Load bitmap textures into VRAM
 			gameBackgroundTexture.load();
+			textBoxTexture.load();
 
 			circleBTA.load();
 			enemyBTA.load();
